b1c4477b6ed1ab0c83b3eb470292cf17cbc6cf22
Jennifer Chisnell <jenniferchisnell@gmail.com>
2012-09-30 18:08:17 -0400

    first commit

---
+++ exercise1.php
@@ -0,0 +1,11 @@
+<?php
+$Name = 'Jennifer Chisnell';
+$DOB = 'July 10, 1985';
+$FavCol = 'blue';
+
+print "My name is $Name";
+print "<br />";
+print "I was born $DOB";
+print "<br />"; 
+print "My favorite color is $FavCol"
+?>
---
+++ exercise2.php
@@ -0,0 +1,10 @@
+<?php 
+$DaysPerWeek = 7;
+$HoursPerDay = 24;
+$MinutesPerHour = 60;
+$MinutesPerWeek = $DaysPerWeek*$HoursPerDay*$MinutesPerHour;
+print $DaysPerWeek;
+
+$HoursPerDay = 26;
+print $DaysPerWeek;
+?>
